15f387bda482bd0aaa3bcf9818e6885813c2154d
Marc Hibbins <marchibbins@gmail.com>
2015-07-23 14:34:27 +0100

    Animated scale

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,13 @@ var Dollar = (function($) {
         },
         classes: {
             active: 'active',
-            hidden: 'hidden'
+            hidden: 'hidden',
+            layer: 'layer',
+            scale: 'scale'
         }
     },
     dom = {},
+    transitioning = false,
 
     init = function () {
         setup();
@@ -49,21 +52,27 @@ var Dollar = (function($) {
 
     attach = function () {
         q('[data-target]').each(function (i, el) {
-            var el = $(el),
+            var el = q(el),
                 targetId = q(el).attr('data-target'),
                 target = q('#' + targetId);
 
             el.on('mouseenter', function () {
-                target.fadeIn();
+                if (!transitioning) {
+                    target.stop().fadeIn();
+                }
             })
 
             .on('mouseleave', function () {
-                target.fadeOut();
+                if (!transitioning) {
+                    target.stop().fadeOut();
+                }
             })
 
             .on('click', function (event) {
                 event.preventDefault();
-                selectSection(el, target);
+                if (!transitioning) {
+                    selectSection(el, target);
+                }
             });
         });
     },
@@ -71,15 +80,38 @@ var Dollar = (function($) {
     selectSection = function (el, target) {
         resetToggles();
         el.unbind('mouseenter mouseleave');
-        target.addClass(config.classes.active);
-        target.removeClass(config.classes.hidden);
+
+        target.stop().fadeTo(0, 100)
+            .addClass(config.classes.active)
+            .removeClass(config.classes.hidden);
+
+        if (!dom.container.hasClass(config.classes.scale)) {
+            resizeBackgrounds();
+        }
     },
 
     resetToggles = function () {
         $(dom.sections).each(function (i, section) {
             if (!section.hasClass(config.classes.active)) {
-                section.addClass(config.classes.hidden);
+                section.stop().fadeTo(0, 0).addClass(config.classes.hidden);
             }
+        });
+    },
+
+    resizeBackgrounds = function () {
+        dom.container.addClass(config.classes.scale);
+        var animProperties = {
+                backgroundSize: '900px',
+                backgroundPositionX: -150,
+                backgroundPositionY: -410
+            },
+            duration = 1000,
+            ease = 'easeInOutQuart';
+
+        transitioning = true;
+        dom.container.animate(animProperties, duration, ease);
+        q('.' + config.classes.layer).animate(animProperties, duration, ease, function() {
+            transitioning = false;
         });
     };
 
